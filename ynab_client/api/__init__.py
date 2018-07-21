020b6e5b02fe93b9eed845730e22620e667c1593
Gordon Chiam <gchiam@zendesk.com>
2018-07-22 02:44:12 +0800

    Rename swagger_client package to ynab_client

---
+++ ynab_client/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ynab_client.api.accounts_api import AccountsApi
+from ynab_client.api.budgets_api import BudgetsApi
+from ynab_client.api.categories_api import CategoriesApi
+from ynab_client.api.months_api import MonthsApi
+from ynab_client.api.payee_locations_api import PayeeLocationsApi
+from ynab_client.api.payees_api import PayeesApi
+from ynab_client.api.scheduled_transactions_api import ScheduledTransactionsApi
+from ynab_client.api.transactions_api import TransactionsApi
+from ynab_client.api.user_api import UserApi
